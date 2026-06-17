60cd3b18ea1a4834455de84d86fb4b310a54d445
Daniel Genov <danigenov@protonmail.com>
2026-06-17 18:02:33 +0200

    feat: add main routes

---
+++ src/Components/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <Page>
+      <h1>Hi</h1>
+    </Page>
+  );
+}
+
+const Page = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: var(--bg-main);
+`;
+
+export default Login;
