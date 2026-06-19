4bac91681b02276a5327004144508b2062883e49
Daniel Genov <danigenov@protonmail.com>
2026-06-19 22:28:45 +0200

    feat: add container

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,18 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import LogoPng from "/logo_provisional.png";
 
 function Login() {
   return (
-    <Page>
-      <h1>Hi</h1>
-    </Page>
+    <Container>
+      <Logo>
+        <img src={LogoPng} />
+      </Logo>
+      <FormTitle>
+        <h1>BookPop</h1>
+      </FormTitle>
+    </Container>
   );
 }
 
-const Page = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  background-color: var(--bg-main);
+const Container = styled.div`
+  width: 80%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
+  margin-top: 200px;
+  background-color: white;
 `;
 
+const Logo = styled.div`
+  width: 30%;
+  height: 100px;
+  background-color: blue;
+  padding: 0;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    background-color: green;
+  }
+`;
+
+const FormTitle = styled.div`
+  width: 100%;
+  text-align: center;
+  h1 {
+    margin: 0;
+  }
+`;
 export default Login;
